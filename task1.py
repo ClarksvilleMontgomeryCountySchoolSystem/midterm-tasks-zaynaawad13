9e94e0bc7623e70ff42d69f0f5a7b17a19c86326
zaynaawad13 <zayna.awad725@students.cmcss.net>
2025-12-18 08:26:02 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,23 @@
+slices = party_pizza_mini + large + medium
+print("Total number of slices: {slices}")
 
+people  += 1
+share = slices // people
+leftover = slices % people
+print("Each person gets: {share}")
+print("Leftover slices: {leftover}")
+
+people += 2
+share = slices // people
+leftover = slices % people
+print ("Each person gets : {share}")
+print ("Leftover slices: {leftover}")
+
+
+
+slices += 14
+share = slices // people
+leftover = slices % people
+print("Each person gets: {share}")
+print("Leftover slices: {leftover}")
+print("...for Mr. Hollow Leg")
