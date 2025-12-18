33b7ac707e4c8ab8f585a90b9c3795fb2e9ecd79
zaynaawad13 <zayna.awad725@students.cmcss.net>
2025-12-18 08:50:10 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,15 @@
+# Week 1: Chores and first purchase
+allowance += dishes
+allowance += lawn
+allowance -= candy
 
+# Week 2: Bonus week and purchase
+allowance *= 3
+allowance += vacuum
+allowance -= toy
+
+# Week 3: Savings
+allowance /= 2
+
+# Print final allowance
+print(f"Allowance: ${allowance}")
